533f6dbcda7301c43b0ea2d2ccd32d4422fc9cd5
ObjQIAN <790388448@qq.com>
2023-09-24 15:25:17 -0400

    Start building
    Start building and changed tile layer

--- templates/scrollytelly/js/index.js
+++ templates/scrollytelly/js/index.js
@@ -3,8 +3,8 @@ import { SlideDeck } from './slidedeck.js';
 const map = L.map('map', {scrollWheelZoom: false}).setView([0, 0], 0);
 
 // ## The Base Tile Layer
-L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+L.tileLayer('http://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://www.thunderforest.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
 }).addTo(map);
 
 // ## Interface Elements
