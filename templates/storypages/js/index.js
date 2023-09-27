c41cd16eaae3556f82060a190f217622c6eff354
junyi2022 <junyi2022@berkeley.edu>
2023-09-27 13:25:55 -0400

    change slide location

--- templates/storypages/js/index.js
+++ templates/storypages/js/index.js
@@ -4,7 +4,7 @@ const map = L.map('map').setView([0, 0], 0);
 
 // ## The Base Tile Layer
 const baseTileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/junyiy/clmxo8j1605wd01qr7dh827m5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianVueWl5IiwiYSI6ImNsbXMza292bjAxcXoybG1meHhuZ3N1cjYifQ.EYo5VECxk9-NCAEgc3dm9w', {
-  attribution: 'Map tiles by <a href="https://www.mapbox.com/">Mapbox</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+  attribution: `© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>`,
 });
 baseTileLayer.addTo(map);
 
