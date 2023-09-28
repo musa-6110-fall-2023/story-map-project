417642397d9efc7e13796c33c8093927d92b463f
seuha <144240775+seuha@users.noreply.github.com>
2023-09-27 21:04:29 -0400

    my story map push

---
+++ templates/Alice StoryMap/js/index.js
@@ -0,0 +1,23 @@
+import { SlideDeck } from './slidedeck.js';
+
+const map = L.map('map', {scrollWheelZoom: false}).setView([0, 0], 0);
+
+// ## The Base Tile Layer
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(map);
+
+// ## Interface Elements
+const slides = document.querySelectorAll('.slide');
+
+
+
+// ## The SlideDeck object
+const deck = new SlideDeck(slides, map);
+
+document.addEventListener('scroll', () => deck.calcCurrentSlideIndex());
+
+deck.preloadFeatureCollections();
+deck.syncMapToCurrentSlide();
