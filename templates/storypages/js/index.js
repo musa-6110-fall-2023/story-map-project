b3a911bd2de821d1490126ad137da23a5a7c3b19
watson <wx7@upenn.edu>
2023-10-02 14:43:04 -0400

    first commit from watson

--- templates/storypages/js/index.js
+++ templates/storypages/js/index.js
@@ -3,8 +3,11 @@ import { SlideDeck } from './slidedeck.js';
 const map = L.map('map').setView([0, 0], 0);
 
 // ## The Base Tile Layer
-const baseTileLayer = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+const baseTileLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 });
 baseTileLayer.addTo(map);
 
